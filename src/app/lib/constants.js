ab686766827f6fad5e86bc7e43f792bd9cc35167
RyanL2004 <rayanlouahche2004@gmail.com>
2025-01-05 01:08:17 +0000

    Refactor constants & utils

--- src/app/lib/constants.js
+++
@@ -1,9 +0,0 @@
-// src/lib/constants.js
-export const TRANSLATION_TYPES = {
-    GENERAL: 'general',
-    PROFESSIONAL: 'professional',
-    SCIENTIFIC: 'scientific',
-    DOCUMENTS: 'documents',
-    CONVERSATION: 'conversation'
-  }
-
